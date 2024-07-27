4ce68baddffc2bd86c1934fd3d9724d8a04be934
sounar97 <souravnarayan97@gmail.com>
2024-07-28 00:29:27 +0530

    connected to database

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express=require('express');
+const mongoose=require('mongoose');
 const app=express();
 
-app.listen(3000,()=>{console.log('Server is running on port 3000')});
+
 
 app.get('/',(req,res)=>{
     res.send('Hello from node api server');
 });
+
+mongoose.connect("mongodb+srv://sounar345:uum5oJb3D5M0fDfD@backend.vb4j5t4.mongodb.net/?retryWrites=true&w=majority&appName=Backend")
+.then(()=>{console.log('Connected to database')
+app.listen(3000,()=>{console.log('Server is running on port 3000')})})
+.catch((err)=>{console.log('Error connecting to database')});

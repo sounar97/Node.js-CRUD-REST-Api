b951f2b32f4e9dcf651a264f46c44569c92be0a2
sounar97 <souravnarayan97@gmail.com>
2024-07-28 23:41:38 +0530

    sending request

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const express=require('express');
 const mongoose=require('mongoose');
 const app=express();
 
-
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send('Hello from node api server');
 });
 
+app.post('/api/products',(req,res)=>{
+    console.log(req.body);
+    res.send(req.body);
+});
+
 mongoose.connect("mongodb+srv://sounar345:uum5oJb3D5M0fDfD@backend.vb4j5t4.mongodb.net/?retryWrites=true&w=majority&appName=Backend")
 .then(()=>{console.log('Connected to database')
-app.listen(3000,()=>{console.log('Server is running on port 3000')})})
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000')}
+)})
 .catch((err)=>{console.log('Error connecting to database')});

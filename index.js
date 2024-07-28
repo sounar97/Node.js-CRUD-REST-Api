9f0c5254b7d39fa2cee9b4fae8999d476554e8b8
sounar97 <souravnarayan97@gmail.com>
2024-07-29 01:05:30 +0530

    test1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const Product=require('./models/product.model.js');
 const app=express();
 
 app.use(express.json());
@@ -8,9 +9,23 @@ app.get('/',(req,res)=>{
     res.send('Hello from node api server');
 });
 
-app.post('/api/products',(req,res)=>{
-    console.log(req.body);
-    res.send(req.body);
+
+app.post('/api/products',async(req,res)=>{
+    try{
+         const product= await Product.create(req.body);
+         res.status(200).json(product);
+    }catch(error){
+        res.status(500).send({message:error.message})
+    }
+});
+app.get('/api/products',async(req,res)=>{
+    try{
+            const product=await Product.find();
+            res.status(200).json(product);
+    }
+    catch(error){
+      res.status(500).send({message:error.message});
+    }
 });
 
 mongoose.connect("mongodb+srv://sounar345:uum5oJb3D5M0fDfD@backend.vb4j5t4.mongodb.net/?retryWrites=true&w=majority&appName=Backend")

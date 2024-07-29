daf54ca2fd26807b069fd276a50f300b201eb33b
sounar97 <souravnarayan97@gmail.com>
2024-07-29 16:42:25 +0530

    finding the product by id

--- index.js
+++ index.js
@@ -28,9 +28,19 @@ app.get('/api/products',async(req,res)=>{
     }
 });
 
+app.get('/api/products/:id',async(req,res)=>{
+    try{
+        const product=await Product.findById(req.params.id);
+        res.status(200).json(product);
+    }
+    catch(error){
+        res.status(500).send({message:error.message});
+    }
+});
+
 mongoose.connect("mongodb+srv://sounar345:uum5oJb3D5M0fDfD@backend.vb4j5t4.mongodb.net/?retryWrites=true&w=majority&appName=Backend")
-.then(()=>{console.log('Connected to database')
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000')}
-)})
+.then(()=>{
+    console.log('Connected to database')
+    app.listen(3000,()=>{
+    console.log('Server is running on port 3000')})})
 .catch((err)=>{console.log('Error connecting to database')});

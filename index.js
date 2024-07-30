eddd517c317bb25be77cb9cb76c50b576a6a6edf
sounar97 <souravnarayan97@gmail.com>
2024-07-30 19:08:59 +0530

    updating index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     res.send('Hello from node api server');
 });
 
-mongoose.connect("mongodb+srv://sounar345:uum5oJb3D5M0fDfD@backend.vb4j5t4.mongodb.net/?retryWrites=true&w=majority&appName=Backend")
+mongoose.connect("")
     .then(() => {
         console.log('Connected to database')
         app.listen(3000, () => {
